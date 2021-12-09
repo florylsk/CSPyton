8f82126187d403421b9305f37490534564fc4af8
florylsk <florilsk@yahoo.es>
2021-12-09 12:13:58 +0100

    sin puertos

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+import constraint
 from constraint import *
 import sys
 from constraint import *
+import numpy as np
 #Todo
 # if len(sys.argv) != 4:
 #     sys.exit("Invalid amount of arguments to start the program")
-#
 # path = sys.argv[1]
 # map_path = sys.argv[2]
 # container_path = sys.argv[3]
@@ -15,25 +16,50 @@ array_contenedores=[]
 with open("mapa.txt") as textFile:
     lines = [line.split() for line in textFile]
     array_mapa=lines
-    for line in lines:
-        print(line)
 
 #TODO: cambiar esto para que use container_path
 with open("contenedores.txt") as textFile:
     lines = [line.split() for line in textFile]
     array_contenedores=lines
-    for line in lines:
-        print(line)
 
+new_array_mapa=[]
+MapaShape=np.shape(array_mapa)
+for len in range(0,MapaShape[0]):
+    new_array_mapa.append([])
+
+for row in range(0,MapaShape[0]):
+    for column in range(0,MapaShape[1]):
+        new_array_mapa[row].append(array_mapa[row][column]+str(row)+str(column))
+
+print("Array final del mapa")
+for row in new_array_mapa:
+    print(row)
+
+new_array_contenedores=[]
+ContendoresShape=np.shape(array_contenedores)
+for row in range(0,ContendoresShape[0]):
+    new_array_contenedores.append(array_contenedores[row][0]+array_contenedores[row][1]+array_contenedores[row][2])
+print("Array final de contenedores")
+print(new_array_contenedores)
 
 problem = Problem()
-problem.addVariables('S',['E','N'])
-problem.addVariables('R',['E'])
 
+dominioE=[]
+dominioN=[]
+newArrMapaShape=np.shape(new_array_mapa)
+for row in range(0,newArrMapaShape[0]):
+    for column in range(0,newArrMapaShape[1]):
+        if 'E' in new_array_mapa[row][column]:
+            dominioE.append(new_array_mapa[row][column])
+        elif 'N' in new_array_mapa[row][column]:
+            dominioN.append(new_array_mapa[row][column])
 
-def validPosition(a):
-   if a != "X":
-        return True
-problem.addConstraint(validPosition, ('S'))
-problem.addConstraint(validPosition, ('R'))
-print(problem.getSolutions())
+for variable in new_array_contenedores:
+    if 'S' in variable:
+        problem.addVariable(variable,dominioN+dominioE)
+    elif 'R' in variable:
+        problem.addVariable(variable,dominioE)
+problem.addConstraint(constraint.AllDifferentConstraint())
+print("------------------------------------------------------------------------------------------------------------------------------------------------")
+print("Solucion final")
+print(problem.getSolution())
