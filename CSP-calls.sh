e4d14ddb26edb353c940521498af8a70d4705e93
florylsk <florilsk@yahoo.es>
2021-12-15 07:02:46 +0100

    SH

--- CSP-calls.sh
+++ CSP-calls.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-./CSPStowage.py CSP-tests mapa1 contenedores1
-./CSPStowage.py CSP-tests mapa2 contenedores2
-./CSPStowage.py CSP-tests mapa3 contenedores3
-./CSPStowage.py CSP-tests mapa4 contenedores4
-./CSPStowage.py CSP-tests mapa5 contenedores5
+python3 CSPStowage.py CSP-tests mapa1 contenedores1
+python3 CSPStowage.py CSP-tests mapa2 contenedores2
+python3 CSPStowage.py CSP-tests mapa3 contenedores3
+python3 CSPStowage.py CSP-tests mapa4 contenedores4
+python3 CSPStowage.py CSP-tests mapa5 contenedores5
