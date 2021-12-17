ff8457c7835df4e9460d3e4755e7225260d131f3
florylsk <florilsk@yahoo.es>
2021-12-17 10:13:04 +0100

    WIP AStar cargar

--- ASTARStowage.py
+++ ASTARStowage.py
@@ -7,45 +7,141 @@ import pandas as pd
 import copy
 
 #Te pasan contenedores, inicial->puerto1->puerto2,descaga o carga de contenedores, busqueda de contenedores del puerto X e ir sacandolos
-
-def carga(altura, costeAcumulado):
-    return 10+altura+costeAcumulado
-
-def descarga(altura, costeAcumulado):
-    return (10+(altura*2))+costeAcumulado
-
-def navegar(costeAcumulado):
-    return 3500+costeAcumulado
-
+mapa=    [['N','N','N','N'],
+          ['N','N','N','N'],
+          ['E','N','N','E'],
+          ['X','E','E','X'],
+          ['X','X','X','X']]
+mapaShape=np.shape(mapa)
 class Node:
-    def __init__(self,state,parent=None,position=None):
+    def __init__(self,state,parent=None,position=None,g=0,h=0,move='start'):
         self.state=state
         self.parent=parent
         self.position=position
-        self.g=0
-        self.f=0
-        self.h=0
+        self.g=g
+        self.h=h
+        self.f=g+h
+        self.move=move
 
     def __eq__(self, other):
         return self.position==other.position
 
-def aStar(start,startPort,end, endPort):
-    while(startPort!=endPort):
-        pass
-    pass
+    #accion para cargar un contenedores disponible en el mapa
+    def cargar(self):
+        #if no available containers to deploy or port isnt 0 return empty node
+        if len(self.state[0])==0 or self.state[4]==0:
+            return None
+        #get first container available
+        container=self.state[0][0]
+        #traverse map
+        for row in range(0, mapaShape[0]):
+            for column in range(0, mapaShape[1]):
+                if container.tipo=='S':
+                    if mapa[row][column] == 'N' or mapa[row][column] == 'E':
+                        # if no loaded containers yet
+                        if len(self.state[1])==0:
+                            container.fila=row
+                            container.columna=column
+                            cargar=copy.deepcopy(self.state)
+                            #remove first container from the available containers
+                            cargar[0].pop(0)
+                            cargar[1].append(container)
+                            return Node(cargar,parent=self,position=self.position+1,g=self.g+1,h=self.h+(10+container.fila),move='CargarContainer-'+container.id+container.tipo+container.puerto+"-EnPos-"+container.fila+container.columna)
+                        else:
+                            #for container in loaded containers
+                            for c in self.state[1]:
+                                #if container already lodaded continue the for loop
+                                if c.fila==row and c.columna==column:
+                                    continue
+                            container.fila = row
+                            container.columna = column
+                            cargar = copy.deepcopy(self.state)
+                            cargar[0].pop(0)
+                            cargar[1].append(container)
+                            return Node(cargar, parent=self, position=self.position + 1, g=self.g + 1,h=self.h+(10+container.fila), move='CargarContainer-' + container.id + container.tipo + container.puerto + "-EnPos-" + container.fila + container.columna)
+                elif container.tipo=='R':
+                    if mapa[row][column] == 'E':
+                        # if no loaded containers yet
+                        if len(self.state[1])==0:
+                            container.fila=row
+                            container.columna=column
+                            cargar=copy.deepcopy(self.state)
+                            cargar[0].pop(0)
+                            cargar[1].append(container)
+                            return Node(cargar,parent=self,position=self.position+1,g=self.g+1,h=self.h+(10+container.fila),move='CargarContainer-'+container.id+container.tipo+container.puerto+"-EnPos-"+container.fila+container.columna)
+                        else:
+                            #for container in loaded containers
+                            for c in self.state[1]:
+                                #if container already lodaded continue the for loop
+                                if c.fila==row and c.columna==column:
+                                    continue
+                            container.fila = row
+                            container.columna = column
+                            cargar = copy.deepcopy(self.state)
+                            cargar[0].pop(0)
+                            cargar[1].append(container)
+                            return Node(cargar, parent=self, position=self.position + 1, g=self.g + 1,h=self.h+(10+container.fila), move='CargarContainer-' + container.id + container.tipo + container.puerto + "-EnPos-" + container.fila + container.columna)
+
+
+
+
+
+
+
+
+
+
+
+class Container:
+
+
+    def __init__(self,id,tipo,puerto,fila=-1,columna=-1):
+        self.id=id
+        self.tipo=tipo
+        self.puerto=puerto
+        self.fila=fila
+        self.columna=columna
+
+
+class AStar:
+    def __init__(self,start,end):
+        self.start=start
+        self.end=end
+        self.mapa=mapa
+        self.open=[]
+        self.closed=[]
+
+    def solve(self):
 
 
 
 def main():
-    #codification: 0=X,1=N,2=E,4=R1,5=S1,6=R2,7=S2
-    start=[[1,1,1,1],
-          [1,1,1,1],
-          [2,2,2,2],
-          [0,2,2,0],
-          [0,0,0,0]]
-    start=Node(state=start)
-    end=
-    aStar(start,end)
+    #estado=contenedoresDisponibles,contenedoresCargados,contenedoresDescargados1,contenedoresDescargados2,puertoActual
+    container1=Container(1,'S',1)
+    container2=Container(2,'S',1)
+    container3=Container(3,'S',1)
+    container4=Container(4,'R',2)
+    container5=Container(5,'R',2)
+
+
+    _start=[
+           [container1,container2,container3,container4,container5],
+            [],
+           [],
+           [],
+           0
+           ]
+    start=Node(state=_start)
+    _end=[
+           [],
+            [],
+           [container1,container2,container3],
+           [container4,container5],
+           2
+           ]
+    end=None(state=_end)
+    solver=AStar(start,end)
+    solver.solve()
 
 
 
